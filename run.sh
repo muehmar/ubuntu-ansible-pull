4436701e62c74833291898ae6f3c5a1b677ef725
Martin Mühlemann <poelis@gmail.com>
2025-10-16 22:05:30 +0200

    add claude code

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Ensure required collections (community.general) are installed
+ansible-galaxy collection install -r requirements.yml
+
 sudo ansible-pull -U https://github.com/muehmar/ubuntu-ansible-pull.git
